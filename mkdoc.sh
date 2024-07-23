7d4c80848a2341181c5c8b2f13e55cde7af35109
jul <julien@tayon.net>
2024-07-23 13:34:21 +0200

    Adding a nnice css from https://gist.github.com/killercup/5917178

--- mkdoc.sh
+++ mkdoc.sh
@@ -22,11 +22,12 @@ pandoc -f gfm  -s index.md -o ../index.pdf.md
 
 rm *md
 cat ../??_*.md  > ../index.pdf.md
+cp ../pandoc.css .
 cat ../titre_md ../index.pdf.md >> ./livre.md
 
 
 pandoc livre.md --toc -V --pdf-engine=xelatex -V papersize=A4 -V documentclass=report -V mainfont="Bookman" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt   -o mon_livre.pdf
-pandoc livre.md --toc --standalone -s  -o index.html
+pandoc livre.md --toc --standalone -s -c pandoc.css  -o index.html
 
 rm ../index.*.md
 ln -s $( realpath ./livre.md ) `pwd`/../README.md
