edef43c07f8d18ff0f33d04ad7e1532a835f8723
jul <julien@tayon.net>
2024-07-29 04:24:36 +0200

    latex et fonte lisible inclusion de babel pour avoir la table des matière traduite en français inclusion d'un gabarit généré avec une fonte conçue par l'institut braille pour être plus lisible

---
+++ 44_accessiblite.md
@@ -0,0 +1,14 @@
+# Accessibilité
+
+En sortant, l'autre jour, je suis tombé sur quelqu'un qui aurait bien
+aimé lire, une épreuve faite avec ce gabarit, mais ... 
+La fonte et sa taille n'aidait pas.
+
+Je me dis que ce serait bien qu'en tant qu'informaticien, j'essaie d'aider
+un peu donc j'ai fait mes recherche et j'ai rajouté une sortie avec une 
+[fonte conçue par l'institut braille](https://brailleinstitute.org/freefont) pour faciliter la lecture pour les dys-voyants et mal-voyants.
+
+Fonte qu'il va vous falloir télécharger et installer si vous voulez
+générer le livre en format « lisible ».
+
+
--- mkdoc.sh
+++ mkdoc.sh
@@ -26,7 +26,14 @@ cp ../pandoc.css .
 cat ../titre_md ../index.pdf.md >> ./livre.md
 
 
-pandoc livre.md --toc -V --pdf-engine=xelatex -V papersize=A4 -V documentclass=report -V mainfont="Bookman" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt   -o mon_livre.pdf
+pandoc livre.md --toc  --pdf-engine=xelatex -V papersize=A4 -V documentclass=report --variable fontsize=12pt   -o mon_livre.pdf
+pandoc livre.md  --toc --pdf-engine=xelatex  -V papersize=A4 \
+    -V documentclass=report   --variable fontsize=12pt \
+    -V "mainfont:AtkinsonHyperlegible" \
+    -V "mainfontoptions:Extension=.ttf, UprightFont=*-Regular, BoldFont=*-Bold,  ItalicFont=*-Italic, BoldItalicFont=*-BoldItalic" \
+    -so livre_lisible.pdf
+
+
 pandoc livre.md --toc --standalone -s -c pandoc.css  -o index.html
 
 rm ../index.*.md
